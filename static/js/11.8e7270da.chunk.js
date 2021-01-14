9490e352285a46bc90a9c820efdf6d8700504b35
elastic-master <inertia.dev20@gmail.com>
2021-01-14 12:12:51 +1100

    Updates

--- static/js/11.8e7270da.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpinertia-website"]=this["webpackJsonpinertia-website"]||[]).push([[11],{175:function(e,t,i){"use strict";i.r(t);var s=i(1);i(0);t.default=function(){return Object(s.jsx)("div",{className:"Bounty Page"})}}}]);
-//# sourceMappingURL=11.8e7270da.chunk.js.map
---
+++ static/js/12.35546a70.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpinertia-website"]=this["webpackJsonpinertia-website"]||[]).push([[12],{179:function(t,e,i){"use strict";i.r(e);var s=i(1);i(0);e.default=function(){return Object(s.jsx)("div",{className:"Contact Page"})}}}]);
+//# sourceMappingURL=12.35546a70.chunk.js.map
